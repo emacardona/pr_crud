e64c55c9cc0c9271e834250386332a7def2800a1
emacardona <ediaz3157@gmail.com>
2024-09-01 19:12:25 -0600

    Add files via upload

---
+++ crud.sql
@@ -0,0 +1,7 @@
+SELECT * FROM db_empresa1.clientes;
+
+
+INSERT INTO clientes(`nit`,`nombres`,`apellidos`,`direccion`,`telefono`,`fecha_nacimiento`) VALUES ();
+
+
+UPDATE clintes SET nit = ?, nombres = ?, apellidos = ?, direccion = ?, telefono = ?, fecha_nacimiento = ? WHERE id_cliente = ?;
